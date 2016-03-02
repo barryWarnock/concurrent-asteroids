f957b7cdf9073b44a6de21e7b131c1ebdab0b58f
JeremyBolster <bolster@Jeremys-MacBook-Pro.local>
2016-03-02 15:36:39 -0800

    Building Quad Tree

--- src/collision/QuadTreeNode.java
+++ src/collision/QuadTreeNode.java
@@ -1,8 +1,17 @@
 package collision;
 
-public class QuadTree {
+public class QuadTreeNode {
 	
-	private TreeNode root;
+	private QuadTreeNode child_1;
+	private QuadTreeNode child_2;
+	private QuadTreeNode child_3;
+	private QuadTreeNode child_4;
 	
+	public QuadTreeNode() {
+		child_1 = null;
+		child_2 = null;
+		child_3 = null;
+		child_4 = null;
+	}
 	
 }
