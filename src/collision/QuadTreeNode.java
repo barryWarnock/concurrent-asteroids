1c6b2e227e16f52108d5b26f043748563d3fd8ab
bolster <bolster@unbc.ca>
2016-03-14 15:12:01 -0700

    Fixing merge issues

--- src/collision/QuadTreeNode.java
+++ src/collision/QuadTreeNode.java
@@ -1,16 +1,17 @@
 package collision;
 
 public class QuadTreeNode<T> {
+
 	int x, y, width, heigth;
 
-	private ArrayList<Entity> entities;
+	private List<Entity> entities;
 
 	private QuadTreeNode<T> child_1;
 	private QuadTreeNode<T> child_2;
 	private QuadTreeNode<T> child_3;
 	private QuadTreeNode<T> child_4;
 	
-	public QuadTreeNode<T>(int x, int y, int width, int height) {
+	public QuadTreeNode(int x, int y, int width, int height) {
 		child_1 = null;
 		child_2 = null;
 		child_3 = null;
