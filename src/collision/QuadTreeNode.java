0bcec8c32b73755faf0485d57173306c7bfd26f2
bolster <bolster@unbc.ca>
2016-03-14 15:28:49 -0700

    Fixed Conflicts?

--- src/collision/QuadTreeNode.java
+++ src/collision/QuadTreeNode.java
@@ -1,18 +1,27 @@
 package collision;
 
 public class QuadTreeNode<T> {
-	
+
+	int x, y, width, heigth;
+
+	private List<Entity> entities;
+
 	private QuadTreeNode<T> child_1;
 	private QuadTreeNode<T> child_2;
 	private QuadTreeNode<T> child_3;
 	private QuadTreeNode<T> child_4;
 	
-	public QuadTreeNode() {
+	public QuadTreeNode(int x, int y, int width, int height) {
 		child_1 = null;
 		child_2 = null;
 		child_3 = null;
 		child_4 = null;
+		
+		this.x = x;
+		this.y = y;
+		this.width  = w;
+		this.height = h;
+
+		entities = new ArrayList();
 	}
-	
-	
 }
