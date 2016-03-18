18d473b5d0c9a00f6c7a8a711bd2973449536685
bolster <bolster@unbc.ca>
2016-03-18 11:38:29 -0700

    Added a few this to entities.

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -6,6 +6,15 @@ public abstract class Entity {
 	
 	protected int xPos;
 	protected int yPos;
+	protected int xSpeed;
+	protected int ySpeed;
+
+	public Entity() {
+		xPos = 0;
+		yPos = 0;
+		xSpeed = 0;
+		ySpeed = 0;
+	}
 	
 	protected List<Entity> potentialCollisions;
 	
@@ -14,5 +23,15 @@ public abstract class Entity {
 	}
 	
 	public abstract void runCollisionChecking();
+
+	public abstract void updateLocationAndMomentum();
+
+	/**
+	 * This is a very naive
+	 * @return Asteroid momentum
+	 */
+	protected int momentum() {
+		return (xSpeed + ySpeed);
+	}
 
 }
