f3e6349723c2931e47c695093722130981643e83
Mitch <mitchcarter@live.ca>
2016-03-19 13:55:17 -0700

    Update to player, basic movement and input from WAD implemented

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,12 +1,30 @@
 package entity;
 
-public class Player extends Entity {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import static java.awt.event.KeyEvent.VK_A;
+import static java.awt.event.KeyEvent.VK_D;
+import static java.awt.event.KeyEvent.VK_W;
+
+public class Player extends Entity implements KeyListener{
 
 	private static Player player = new Player();
 
+	private int degOfRotation = 0;
+	//0 degrees corresponds to facing directly vertical
+
+	private static int linearSpeedChange = 1;
+	//speed increase in direction player is moving
+
 	private Player(){
 	}
-	
+
+	@Override
+	public void runCollisionChecking() {
+		// TODO
+	}
+
 	public Player getInstance() {
 		return player;
 	}
@@ -17,16 +35,50 @@ public class Player extends Entity {
 		return player;
 	}
 
-	@Override
-	/**
-	 * TODO complete this, this should possibly be done in Enitity.
-	 */
-	public void runCollisionChecking() {
-
-	}
 
 	@Override
 	public void updateLocationAndMomentum() {
-		//TODO
+		xPos += xSpeed;
+		yPos += ySpeed;
 	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int keyPressed = e.getKeyCode();
+
+		int theta;
+
+		switch(keyPressed) {
+			case VK_A: 	degOfRotation += 1;
+				//rotate cclockwise, positive degree increase
+				break;
+			case VK_D: 	degOfRotation -= 1;
+				//rotate clockwise, negative degree increase
+				break;
+			case VK_W: 	theta = degOfRotation%360;
+						xSpeed += (int)(Math.sin((double)theta)*linearSpeedChange);
+						ySpeed += (int)(Math.cos((double)theta)*linearSpeedChange);
+				/*
+				move forward, decompose linear speed change into x and y components
+				based on the coordinate system where 0 degrees is viewed as vertical
+				to the user and x and y axes are swapped relative to Cartesian coordinates
+				*/
+				break;
+			default:
+				break;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+	}
+
+	//bufferedImage Sprite = imageIO.read(getClass().getResource(/graphics.playerMove));
+
 }
