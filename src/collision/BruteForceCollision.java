957aa001dc6ca80560306218c39b9c190ed36a10
bolster <bolster@unbc.ca>
2016-04-02 11:01:07 -0700

    Brute force collision should work now.

--- src/collision/BruteForceCollision.java
+++ src/collision/BruteForceCollision.java
@@ -2,27 +2,41 @@ package collision;
 
 import entity.Entity;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class BruteForceCollision implements CollisionChecker{
+public class BruteForceCollision implements CollisionChecker {
+
     public void checkCollisions(Iterator<Entity> entities) {
 
+        List<Entity> entityList = new ArrayList<>();
 
         while (entities.hasNext()) {
-            Entity current = entities.next();
-
-            //TODO this is horribly broken
-            //The iterator must be cloned or deep copied.
-            //This version will not work at all.
-            Iterator<Entity> otherIt = entities;
-            while(otherIt.hasNext()) {
-                Entity other = otherIt.next();
-                if (current.checkCollision(other)) {
-                    current.reportCollision(other);
-                    other.reportCollision(current);
-                }
-            }
+            entityList.add(entities.next());
         }
+        entities = entityList.iterator();
+
+        while(entities.hasNext()) {
+            Iterator<Entity> tempList = entityList.iterator();
+            Entity ent1 = entities.next();
+
+            while(tempList.hasNext()) {
+                Entity ent2 = tempList.next();
+
+                if(ent1.get_x() < ent2.get_x()) {
+                    if( (ent1.get_x() + ent1.get_width()) >= ent2.get_x()) {
+                        if(ent1.get_y() < ent2.get_y()) {
+                            if( (ent1.get_y() + ent1.get_height()) >= ent2.get_y()) {
+                                ent1.checkCollision(ent2);
+                            }
+                        }
+                    }
+                }
+
+            }
+
+        }
+
     }
 }
